747740fe6a51543f0d655ff72cddf249854ee31b
Rayncor <sowa332@gmail.com>
2023-01-14 13:31:52 -0800

    finished login/out functionality, adding condiitional render

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,17 +7,30 @@ import Image from 'react-bootstrap/Image';
 import oreo from '../assets/images/oreo.jpg';
 import { Link } from 'react-router-dom';
 import '../styles/nav.css';
+import { useState } from 'react';
+import { auth } from '../config/firebase';
+import { useEffect } from 'react';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {signOut} from 'firebase/auth';
 
 
 function NavigationBar() {
+    const [user, loading, error] = useAuthState(auth);
+
+    const signUserOut = async () => {
+        await signOut(auth);
+    }
+
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg='light' expand="lg">
       <Container >
       <Image src={oreo} className='oreoClass'></Image>
-        <Navbar.Brand href="#home">The Corgi Fetcher</Navbar.Brand>
+        <p>{user?.displayName}</p>
+        {/* <Image src= {user?.photoURL || ''}/> */}
+        <Navbar.Brand>The Corgi Fetcher</Navbar.Brand>
         <Nav.Link href='/'><Button variant="primary">Home</Button>{' '}</Nav.Link>
         <Nav.Link href='/login'><Button variant="info">Login</Button>{' '}</Nav.Link>
-        <Nav.Link href='/'><Button variant="danger">Logout</Button>{' '}</Nav.Link>
+        {user && <Nav.Link href='/'><Button onClick={signUserOut} variant="danger">Logout</Button>{' '}</Nav.Link> }
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,11 +4,15 @@ import { signInWithPopup } from 'firebase/auth';
 import Button from 'react-bootstrap/esm/Button';
 import google from '../assets/images/google.jpg'
 import '../styles/login.css'
+import {useNavigate} from 'react-router-dom';
 
 export const Login = () => {
+    const navigate = useNavigate();
+
     const signInWithGoogle = async () => {
         const result = await signInWithPopup(auth, provider) ;
         console.log(result);
+        navigate('/')
     }
 
   return (
