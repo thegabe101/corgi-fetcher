1d0cd5b03594f00b6f5bc534796bf80443c0832c
Rayncor <sowa332@gmail.com>
2023-01-14 12:11:35 -0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import NavigationBar from './components/Nav';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavigationBar/>
+        <Routes>
+          <Route path='/'></Route>
+          <Route path='/login'></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
